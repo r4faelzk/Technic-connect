0c0311210e7448d3965a02d19b76db827b8252a8
r4faelzk <rafaelferreiraoliveira288@gmail.com>
2024-02-16 17:45:38 -0300

    Logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Logo from "../images/image-removebg-preview.png"
 import { Link, NavLink } from "react-router-dom";
 import "./Navbar.css";
 
 export const Navbar = () => {
     return (
         <nav>
+            <a href="
+            " className="logo"><img src={Logo} alt="logo" /></a>
             <ul>
                 <li>
                     <NavLink to="/">Home</NavLink>
